87a36dc760fedc0cc0e33a6c999480fabfc6d898
Jiajun Dai <jdai10@mail.ccsf.edu>
2024-04-20 12:57:25 -0700

    minor fix

--- src/components/dashboard/WeatherBox.jsx
+++ src/components/dashboard/WeatherBox.jsx
@@ -6,60 +6,87 @@ export default function WeatherBox(props) {
   const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${props.latState}&lon=${props.lngState}&appid=${API_KEY}`;
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) => {
-        if(data.weather[0].main === 'Thunderstorm') {
-            data['weather_img_str'] = 'thunderstorm';
-        }else if(data.weather[0].main === 'Drizzle') {
-            data['weather_img_str'] = 'weather_mix';
-        }else if(data.weather[0].main === 'Rain') {
-            data['weather_img_str'] = 'rainy';
-        }else if(data.weather[0].main === 'Snow') {
-            data['weather_img_str'] = 'cloudy_snowing';
-        }else if(data.weather[0].main === 'Clear') {
-            data['weather_img_str'] = 'sunny';
-        }else if(data.weather[0].main === 'Clouds') {
-            data['weather_img_str'] = 'cloud';
-        }else if(data.weather[0].main === 'Squall') {
-            data['weather_img_str'] = 'air';
-        }else if(data.weather[0].main === 'Tornado') {
-            data['weather_img_str'] = 'tornado';
-        }else if(data.weather[0].main === 'Mist' || data.weather[0].main === 'Fog' || data.weather[0].main === 'Haze' || data.weather[0].main === 'Smoke' || data.weather[0].main === 'Dust' || data.weather[0].main === 'Sand' || data.weather[0].main === 'Ash') {
-            data['weather_img_str'] = 'foggy';
-        }
-        setWeatherData(data);
-        console.log(data);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
+    if (props.latState !== null && props.lngState !== null) {
+      fetch(API_URL)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.weather[0].main === "Thunderstorm") {
+            data["weather_img_str"] = "thunderstorm";
+          } else if (data.weather[0].main === "Drizzle") {
+            data["weather_img_str"] = "weather_mix";
+          } else if (data.weather[0].main === "Rain") {
+            data["weather_img_str"] = "rainy";
+          } else if (data.weather[0].main === "Snow") {
+            data["weather_img_str"] = "cloudy_snowing";
+          } else if (data.weather[0].main === "Clear") {
+            data["weather_img_str"] = "sunny";
+          } else if (data.weather[0].main === "Clouds") {
+            data["weather_img_str"] = "cloud";
+          } else if (data.weather[0].main === "Squall") {
+            data["weather_img_str"] = "air";
+          } else if (data.weather[0].main === "Tornado") {
+            data["weather_img_str"] = "tornado";
+          } else if (
+            data.weather[0].main === "Mist" ||
+            data.weather[0].main === "Fog" ||
+            data.weather[0].main === "Haze" ||
+            data.weather[0].main === "Smoke" ||
+            data.weather[0].main === "Dust" ||
+            data.weather[0].main === "Sand" ||
+            data.weather[0].main === "Ash"
+          ) {
+            data["weather_img_str"] = "foggy";
+          }
+          setWeatherData(data);
+        })
+        .catch((error) => console.log("Error fetching data:", error));
+    }
   }, [API_URL, props.latState, props.lngState]);
 
   if (!weatherData) {
-    return <div>Click on a Marker to See Weather Info</div>;
+    return (
+      <div className="font-bold">Click on a Marker to See Weather Info</div>
+    );
   }
+  console.log(props.latState, props.lngState);
 
   return (
     <div className="w-full h-32 p-2 bg-white rounded-lg shadow-xl shadow-blue-gray-900">
       <h2 className="text-lg font-bold mb-2">
-        Current Weather in <span className="text-blue-600">{weatherData.name}</span>
+        Current Weather in{" "}
+        <span className="text-blue-600">{weatherData.name}</span>
       </h2>
       <div className="flex justify-between">
         <p className="text-gray-700">
-          <span className="material-symbols-outlined text-md">{weatherData.weather_img_str}</span>
+          <span className="material-symbols-outlined text-md">
+            {weatherData.weather_img_str}
+          </span>
           {weatherData.weather[0].main}
         </p>
-        {weatherData.rain && <p className="text-gray-600">Rain Vol: {weatherData.rain['1h']}mm/h</p>}
-        {weatherData.snow && <p className="text-gray-600">Snow Vol: {weatherData.rain['1h']}mm/h</p>}
-        <p className="text-gray-600">Temperature: {(weatherData.main.temp - 273.15).toFixed(2)}°C</p>
+        {weatherData.rain && (
+          <p className="text-gray-600">
+            Rain Vol: {weatherData.rain["1h"]}mm/h
+          </p>
+        )}
+        {weatherData.snow && (
+          <p className="text-gray-600">
+            Snow Vol: {weatherData.rain["1h"]}mm/h
+          </p>
+        )}
+        <p className="text-gray-600">
+          Temperature: {(weatherData.main.temp - 273.15).toFixed(2)}°C
+        </p>
       </div>
       <div className="flex justify-between">
-      <p className="text-gray-600">Humidity: {weatherData.main.humidity}%</p>
-      <p className="text-gray-600">Visibility: {weatherData.visibility}m</p>
+        <p className="text-gray-600">Humidity: {weatherData.main.humidity}%</p>
+        <p className="text-gray-600">Visibility: {weatherData.visibility}m</p>
       </div>
       <div className="flex justify-between">
-      <p className="text-gray-600">Wind Speed: {weatherData.wind.speed}m/s, </p>
-      <p className="text-gray-600">Degree: {weatherData.wind.deg}, </p>
-      <p className="text-gray-600">Gust: {weatherData.wind.gust}m/s, </p>
+        <p className="text-gray-600">
+          Wind Speed: {weatherData.wind.speed}m/s,{" "}
+        </p>
+        <p className="text-gray-600">Degree: {weatherData.wind.deg}, </p>
+        <p className="text-gray-600">Gust: {weatherData.wind.gust}m/s, </p>
       </div>
     </div>
   );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -36,7 +36,7 @@ export default function Dashboard() {
     setSelectLat(lat);
     setSelectLng(lng);
   };
-  
+
   return (
     <div className="flex flex-col w-full h-full">
       <div className="flex space-x-5 mb-2">
@@ -84,20 +84,20 @@ export default function Dashboard() {
         <div className="flex flex-col w-full h-full">
           <h1 className="w-auto text-center text-lg font-bold">Notifications:</h1>
           <div className="flex h-80 bg-white overflow-y-scroll shadow-xl shadow-blue-gray-900 ml-5 mb-7 p-2">
-            <ul class="w-96 text-surface dark:text-white">
-              <li class="w-full border-b-2 border-neutral-100 py-2 dark:border-white/10">
+            <ul className="w-96 text-surface dark:text-white">
+              <li className="w-full border-b-2 border-neutral-100 py-2 dark:border-white/10">
                 An item
               </li>
-              <li class="w-full border-b-2 border-neutral-100 py-2 dark:border-white/10">
+              <li className="w-full border-b-2 border-neutral-100 py-2 dark:border-white/10">
                 A second item
               </li>
-              <li class="w-full border-b-2 border-neutral-100 py-2 dark:border-white/10">
+              <li className="w-full border-b-2 border-neutral-100 py-2 dark:border-white/10">
                 A third item
               </li>
-              <li class="w-full border-b-2 border-neutral-100 py-2 dark:border-white/10">
+              <li className="w-full border-b-2 border-neutral-100 py-2 dark:border-white/10">
                 A fourth item
               </li>
-              <li class="w-full py-4">And a fifth one</li>
+              <li className="w-full py-4">And a fifth one</li>
             </ul>
           </div>
           <CustomChart incidents={allIncidents[0]} congestions={allCongestions[0]}/>
