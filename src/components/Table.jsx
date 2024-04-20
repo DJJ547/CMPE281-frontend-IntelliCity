1cc8a3d2059ae6e7ab939ec4fcb94ec14ea3eaaa
yifufang <yffang01@gmail.com>
2024-04-20 01:18:18 -0700

    status update api

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Switch from "react-switch";
 
 export default function Table(props) {
   // State to manage the checked state of the switch
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(props.data.map(item => {if (item.status == 'active') {return true;} else {return false;}}));
 
   // Function to handle switch state change
-  const handleChange = (newChecked) => {
-    setChecked(newChecked);
+  const handleChange = (id,index,newStatus) => {
+    setChecked(prevChecked => {
+      const newChecked = [...prevChecked];
+      newChecked[index] = newStatus;
+      return newChecked;
+    });
+    props.callback(id);
   };
 
   return (
@@ -16,9 +21,11 @@ export default function Table(props) {
         <thead>
           <tr>
             <th className="px-4 py-2">ID</th>
-            <th className="px-4 py-2">Name</th>
-            <th className="px-4 py-2">Location</th>
+            <th className="px-4 py-2">dist_id</th>
+            <th className="px-4 py-2">latitude</th>
+            <th className="px-4 py-2">longitude</th>
             <th className="px-4 py-2">Status</th>
+            <th className="px-4 py-2">Action</th>
           </tr>
         </thead>
         <tbody>
@@ -28,27 +35,30 @@ export default function Table(props) {
               className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}
             >
               <td className="border px-4 py-2 text-center">{item.id}</td>
-              <td className="border px-4 py-2 text-center">{item.name}</td>
-              <td className="border px-4 py-2 text-center">{item.location}</td>
+              <td className="border px-4 py-2 text-center">{item.dist_id}</td>
+              <td className="border px-4 py-2 text-center">{item.latitude}</td>
+              <td className="border px-4 py-2 text-center">{item.longitude}</td>
               <td className="border px-4 py-2 text-center">
-                {item.status}
                 <Switch
-                  checked={checked} // Specify the current state of the switch
-                  onChange={handleChange} // Handle switch state change
+                  checked={checked[index]} // Specify the current state of the switch
+                  onChange={()=>handleChange(item.id, index, !checked[index])} // Handle switch state change
                 />
+              </td>
+              <td className="border px-4 py-2 text-center">
+                <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <div className="flex space-x-5 justify-center m-3">
+      {/* <div className="flex space-x-5 justify-center m-3">
         <button className="w-24 bg-blue-500 text-white font-bold py-2 px-4 rounded">
           Confirm
         </button>
         <button className="w-24 bg-blue-500 text-white font-bold py-2 px-4 rounded">
           Cancel
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/pages/CameraManager.jsx
+++ src/pages/CameraManager.jsx
@@ -1,47 +1,136 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import Map from "../components/Map";
 import ADD from "../medias/plus.png";
 import view from "../medias/view.svg";
-import allCameras from "../mockData/allCameras.json";
+import PopupWindow from "../components/PopupWindow";
 
 const container_height = "65vh";
 const container_width = "55vw";
 
 export default function Dashboard() {
-  function addDevice() {
-    console.log("Add device");
+  const [updateUI, setUpdateUI] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
+  const [Devices, setDevices] = useState({
+    cameras: {
+      0: [],
+      1: [],
+      2: [],
+      3: [],
+      4: [],
+      5: [],
+      6: [],
+      7: [],
+      8: [],
+      9: [],
+      10: [],
+      11: [],
+      12: [],
+    },
+  });
+
+  function ViewPopUp() {
+    setShowPopup(true);
   }
+  const callback = async (id) => {
+    try {
+      const response = await fetch("http://localhost:8000/api/DisableDevice/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ index: id }),
+      });
+      const data = await response.json();
+      console.log("data", data);
+      setUpdateUI(!updateUI);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  useEffect(() => {
+    const fetchDevices = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/api/GetAllDevices", { method: "GET" });
+        const data = await response.json();
+  
+        // Only update the state if the data has changed
+        if (JSON.stringify(data) !== JSON.stringify(Devices)) {
+          setDevices(data);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+  
+    fetchDevices();
+  }, [updateUI]);
+
   return (
     <div className="flex flex-col w-full h-full">
       <div className="flex mb-4 justify-between">
-        <button className="flex items-center justify-center w-60 h-25 bg-white shadow-lg transform transition duration-500 ease-in-out active:scale-90" onClick={addDevice}>
-          <img src={ADD} alt="Camera" className="w-16 h-16 mr-2 shadow-sm p-2"/>
+        <button className="flex items-center justify-center w-60 h-25 bg-white shadow-lg transform transition duration-500 ease-in-out active:scale-90">
+          <img
+            src={ADD}
+            alt="Camera"
+            className="w-16 h-16 mr-2 shadow-sm p-2"
+          />
           ADD
         </button>
-        <button className="flex items-center justify-center w-60 h-25 bg-white shadow-lg">
-          <img src={'https://upload.wikimedia.org/wikipedia/commons/5/5e/Flat_minus_icon_-_red.svg'} alt="Camera" className="w-16 h-16 mr-2 shadow-sm p-2"/>
+        <button
+          className="flex items-center justify-center w-60 h-25 bg-white shadow-lg transform transition duration-500 ease-in-out active:scale-90"
+          onClick={ViewPopUp}
+        >
+          <img
+            src={
+              "https://upload.wikimedia.org/wikipedia/commons/5/5e/Flat_minus_icon_-_red.svg"
+            }
+            alt="Camera"
+            className="w-16 h-16 mr-2 shadow-sm p-2"
+          />
           Delete
         </button>
-
-        <button className="flex items-center justify-center w-60 h-25 bg-white shadow-lg">
-          <img src={'https://upload.wikimedia.org/wikipedia/commons/6/62/Eo_circle_orange_repeat.svg'} alt="Camera" className="w-16 h-16 mr-2 shadow-sm p-2"/>
-          Update
+        <button
+          className="flex items-center justify-center w-60 h-25 bg-white shadow-lg transform transition duration-500 ease-in-out active:scale-90"
+          onClick={ViewPopUp}
+        >
+          <img
+            src={
+              "https://upload.wikimedia.org/wikipedia/commons/6/62/Eo_circle_orange_repeat.svg"
+            }
+            alt="Camera"
+            className="w-16 h-16 mr-2 shadow-sm p-2"
+          />
+          Manage
         </button>
 
-        <button className="flex items-center justify-center w-60 h-25 bg-white shadow-lg">
-          <img src={view} alt="Camera" className="w-16 h-16 mr-2 shadow-sm p-2"/>
+        <button className="flex items-center justify-center w-60 h-25 bg-white shadow-lg transform transition duration-500 ease-in-out active:scale-90">
+          <img
+            src={view}
+            alt="Camera"
+            className="w-16 h-16 mr-2 shadow-sm p-2"
+          />
           View
         </button>
-
       </div>
+      {showPopup && (
+        <PopupWindow
+          onClick={() => setShowPopup(false)}
+          data={Devices.cameras[0]}
+          callback={callback}
+        />
+      )}
       <div className="flex w-auto h-2/3">
-        <Map deviceData={allCameras} container_height={container_height} container_width={container_width}/>
+        <Map
+          deviceData={Devices}
+          container_height={container_height}
+          container_width={container_width}
+        />
         <div className="flex ml-5">
           <div className="flex flex-col w-96 h-96 bg-white shadow-lg">
             <div className="flex justify-between p-2">
               <h2 className="text-lg font-bold">Status</h2>
             </div>
-
           </div>
         </div>
       </div>
